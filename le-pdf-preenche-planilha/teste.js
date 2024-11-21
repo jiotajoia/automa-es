7d4339ae6ad9f4264fb8c91f5ba88cf69d2b7e4c
jiotajoia <bleibeducuz@gmail.com>
2024-11-21 11:27:53 -0300

    script que lê um pdf, compara com os valores de uma coluna especifica e atualiza uma coluna de retorno na planilha de teste

--- le-pdf-preenche-planilha/teste.js
+++ le-pdf-preenche-planilha/teste.js
@@ -2,43 +2,48 @@ const fs = require('fs');
 const XLSX = require('xlsx');
 const pdfParse = require('pdf-parse');
 
+// Função para extrair todo o texto do PDF
 async function extrairTextoPDF(pdfPath) {
     const pdfData = fs.readFileSync(pdfPath);
     const textoPDF = await pdfParse(pdfData);
     return textoPDF.text;
 }
 
-async function compararPlanilhaComPDF(planilhaPath, pdfPath, colunaPlanilha, coluna) {
+// Função para comparação entre PDF e planilha
+async function compararPlanilhaComPDF(planilhaPath, pdfPath, colunaPlanilha, colunaRetorno) {
     
+    // Lê o conteúdo da planilha na aba 1 e extrai convertendo os dados para um json
     const planilha = XLSX.readFile(planilhaPath);
     const aba = planilha.Sheets[planilha.SheetNames[0]];
     const dados = XLSX.utils.sheet_to_json(aba);
 
+    // Extrai o texto do PDF, caso tabela, faz o split
     const texto = await extrairTextoPDF(pdfPath);
     const linhasTabela = texto.split('\n');
 
-    console.log(linhasTabela);
-
-    dados.forEach((linhaPlanilha, idx) => {
+    // Itera sobre cada linha da planilha
+    dados.forEach((linhaPlanilha) => {
+        // por coluna especificada
         const valorPlanilha = linhaPlanilha[colunaPlanilha];
-        let encontrado = false;
-        
+      
         // Verifica se o valor da planilha está em alguma linha da tabela
+        let encontrado = false;
         linhasTabela.forEach(linhaPDF => {
             if (linhaPDF.includes(valorPlanilha)) {
-                // Se encontrado, marca como "Encontrado"
+                // Se encontrado, marca como "true"
                 encontrado = true;
             }
         });
 
+        // Retorno especificado na coluna de retorno especificada
         if (encontrado) {
-            linhaPlanilha[coluna] = 'S';
+            linhaPlanilha[colunaRetorno] = 'S';
         } else {
-            linhaPlanilha[coluna] = 'N';
+            linhaPlanilha[colunaRetorno] = 'N';
         }
     });
 
-
+    // Registra o novo valor na planilha
     const novoAba = XLSX.utils.json_to_sheet(dados);
     planilha.Sheets[planilha.SheetNames[0]] = novoAba;
 
@@ -50,10 +55,9 @@ async function main() {
     const pdfPath = 'teste.pdf';
     const planilhaPath = 'teste.xlsx';
     const colunaPlanilha = 'NOME'; 
-    const coluna = 'RETORNO';
+    const colunaRetorno = 'RETORNO';
 
-    await compararPlanilhaComPDF(planilhaPath, pdfPath, colunaPlanilha, coluna);
+    await compararPlanilhaComPDF(planilhaPath, pdfPath, colunaPlanilha, colunaRetorno);
 }
 
 main();
-
